bd66dff2d525ef6f636d04350614d134f8ddced8
William Easton <williamseaston@gmail.com>
2024-03-20 18:19:33 -0500

    linter changes

--- tests/test_es_version.py
+++ tests/test_es_version.py
@@ -10,19 +10,15 @@ from tests.test_util.es_startup_mocks import mock_es_initialization
 
 
 @pytest.mark.asyncio
-async def test_serverless_true(hass: HomeAssistantType, es_aioclient_mock: AiohttpClientMocker):
+async def test_serverless_true(
+    hass: HomeAssistantType, es_aioclient_mock: AiohttpClientMocker
+):
     """Verify serverless instances are detected."""
 
     es_url = "http://test_serverless_true:9200"
 
-    mock_es_initialization(
-        es_aioclient_mock,
-        es_url,
-        mock_serverless_version=True
-    )
-    config = build_full_config({
-        "url": es_url
-    })
+    mock_es_initialization(es_aioclient_mock, es_url, mock_serverless_version=True)
+    config = build_full_config({"url": es_url})
     gateway = ElasticsearchGateway(config)
     await gateway.async_init()
 
@@ -30,8 +26,11 @@ async def test_serverless_true(hass: HomeAssistantType, es_aioclient_mock: Aioht
 
     await gateway.async_stop_gateway()
 
+
 @pytest.mark.asyncio
-async def test_serverless_false(hass: HomeAssistantType, es_aioclient_mock: AiohttpClientMocker):
+async def test_serverless_false(
+    hass: HomeAssistantType, es_aioclient_mock: AiohttpClientMocker
+):
     """Verify non-serverless instances are detected."""
 
     es_url = "http://test_serverless_false:9200"
@@ -40,9 +39,7 @@ async def test_serverless_false(hass: HomeAssistantType, es_aioclient_mock: Aioh
         es_aioclient_mock,
         es_url,
     )
-    config = build_full_config({
-        "url": es_url
-    })
+    config = build_full_config({"url": es_url})
     gateway = ElasticsearchGateway(config)
     await gateway.async_init()
 
