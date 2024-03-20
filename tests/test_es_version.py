6f3a807d42a490ebc582b9bbe0c5a8295fde9432
William Easton <williamseaston@gmail.com>
2024-03-20 18:19:51 -0500

    add es_version tests for meets_minimum_version

--- tests/test_es_version.py
+++ tests/test_es_version.py
@@ -46,3 +46,39 @@ async def test_serverless_false(
     assert gateway.es_version.is_serverless() is False
 
     await gateway.async_stop_gateway()
+
+
+@pytest.mark.asyncio
+async def test_fails_minimum_version(
+    hass: HomeAssistantType, es_aioclient_mock: AiohttpClientMocker
+):
+    """Verify minimum version function works."""
+
+    es_url = "http://test_serverless_false:9200"
+
+    mock_es_initialization(es_aioclient_mock, es_url, mock_v88_cluster=True)
+    config = build_full_config({"url": es_url})
+    gateway = ElasticsearchGateway(config)
+    await gateway.async_init()
+
+    assert gateway.es_version.meets_minimum_version(8, 10) is False
+
+    await gateway.async_stop_gateway()
+
+
+@pytest.mark.asyncio
+async def test_passes_minimum_version(
+    hass: HomeAssistantType, es_aioclient_mock: AiohttpClientMocker
+):
+    """Verify minimum version function works."""
+
+    es_url = "http://test_serverless_false:9200"
+
+    mock_es_initialization(es_aioclient_mock, es_url, mock_v88_cluster=True)
+    config = build_full_config({"url": es_url})
+    gateway = ElasticsearchGateway(config)
+    await gateway.async_init()
+
+    assert gateway.es_version.meets_minimum_version(7, 10) is True
+
+    await gateway.async_stop_gateway()
