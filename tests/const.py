e29dd437a300f4164821b4825c55de7fa7e947de
William Easton <williamseaston@gmail.com>
2024-03-28 16:57:12 -0500

    Use actual version info from real ES instances

--- tests/const.py
+++ tests/const.py
@@ -87,53 +87,56 @@ CLUSTER_INFO_MISSING_CREDENTIALS_RESPONSE_BODY = {
 }
 
 CLUSTER_INFO_8DOT0_RESPONSE_BODY = {
-    "name": "775d9437a77088",
-    "cluster_name": "home-assistant-cluster",
-    "cluster_uuid": "xtsjNokTQGClXbRibWjxyg",
+    "name": "b33ad024a3eb",
+    "cluster_name": "docker-cluster",
+    "cluster_uuid": "0gTsD5juRwmRElXBCEfk6Q",
     "version": {
         "number": "8.0.0",
+        "build_flavor": "default",
         "build_type": "docker",
-        "build_hash": "00000000",
-        "build_date": "2023-10-31",
+        "build_hash": "1b6a7ece17463df5ff54a3e1302d825889aa1161",
+        "build_date": "2022-02-03T16:47:57.507843096Z",
         "build_snapshot": False,
-        "lucene_version": "9.7.0",
-        "minimum_wire_compatibility_version": "8.0.0",
-        "minimum_index_compatibility_version": "8.0.0",
+        "lucene_version": "9.0.0",
+        "minimum_wire_compatibility_version": "7.17.0",
+        "minimum_index_compatibility_version": "7.0.0",
     },
     "tagline": "You Know, for Search",
 }
 
 
 CLUSTER_INFO_7DOT11_RESPONSE_BODY = {
-    "name": "775d9437a77088",
-    "cluster_name": "home-assistant-cluster",
-    "cluster_uuid": "xtsjNokTQGClXbRibWjxyg",
+    "name": "a6680a08c202",
+    "cluster_name": "docker-cluster",
+    "cluster_uuid": "AlICUdW5Tl2ZxNAA1Mk3fg",
     "version": {
         "number": "7.11.0",
+        "build_flavor": "default",
         "build_type": "docker",
-        "build_hash": "00000000",
-        "build_date": "2023-10-31",
+        "build_hash": "8ced7813d6f16d2ef30792e2fcde3e755795ee04",
+        "build_date": "2021-02-08T22:44:01.320463Z",
         "build_snapshot": False,
-        "lucene_version": "9.7.0",
-        "minimum_wire_compatibility_version": "8.0.0",
-        "minimum_index_compatibility_version": "8.0.0",
+        "lucene_version": "8.7.0",
+        "minimum_wire_compatibility_version": "6.8.0",
+        "minimum_index_compatibility_version": "6.0.0-beta1",
     },
     "tagline": "You Know, for Search",
 }
 
 CLUSTER_INFO_7DOT17_RESPONSE_BODY = {
-    "name": "775d9437a77088",
-    "cluster_name": "home-assistant-cluster",
-    "cluster_uuid": "xtsjNokTQGClXbRibWjxyg",
+    "name": "a8bbdc6f0067",
+    "cluster_name": "docker-cluster",
+    "cluster_uuid": "H2qsoRzORwWNxSlhoUpbig",
     "version": {
         "number": "7.17.0",
+        "build_flavor": "default",
         "build_type": "docker",
-        "build_hash": "00000000",
-        "build_date": "2023-10-31",
+        "build_hash": "bee86328705acaa9a6daede7140defd4d9ec56bd",
+        "build_date": "2022-01-28T08:36:04.875279988Z",
         "build_snapshot": False,
-        "lucene_version": "9.7.0",
-        "minimum_wire_compatibility_version": "8.0.0",
-        "minimum_index_compatibility_version": "8.0.0",
+        "lucene_version": "8.11.1",
+        "minimum_wire_compatibility_version": "6.8.0",
+        "minimum_index_compatibility_version": "6.0.0-beta1",
     },
     "tagline": "You Know, for Search",
 }
@@ -157,18 +160,19 @@ CLUSTER_INFO_SERVERLESS_RESPONSE_BODY = {
 }
 
 CLUSTER_INFO_8DOT11_RESPONSE_BODY = {
-    "name": "775d9437a77088",
-    "cluster_name": "home-assistant-cluster",
-    "cluster_uuid": "xtsjNokTQGClXbRibWjxyg",
+    "name": "640dcce4be79",
+    "cluster_name": "docker-cluster",
+    "cluster_uuid": "R-PPqCZYQTCMvkpGcyL4mA",
     "version": {
         "number": "8.11.0",
+        "build_flavor": "default",
         "build_type": "docker",
-        "build_hash": "00000000",
-        "build_date": "2023-10-31",
+        "build_hash": "d9ec3fa628c7b0ba3d25692e277ba26814820b20",
+        "build_date": "2023-11-04T10:04:57.184859352Z",
         "build_snapshot": False,
         "lucene_version": "9.8.0",
-        "minimum_wire_compatibility_version": "8.11.0",
-        "minimum_index_compatibility_version": "8.11.0",
+        "minimum_wire_compatibility_version": "7.17.0",
+        "minimum_index_compatibility_version": "7.0.0",
     },
     "tagline": "You Know, for Search",
 }
