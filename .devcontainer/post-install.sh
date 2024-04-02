54e34dba133f43bccc2d55efb83db5e56d8286a0
William Easton <williamseaston@gmail.com>
2024-04-02 18:07:34 -0500

    Apply suggestions from code review
    Co-authored-by: Larry Gregory <lgregorydev@gmail.com>

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -11,10 +11,10 @@ poetry config virtualenvs.in-project true
 # Now install all dependencies
 poetry install
 
-if [[ ! -d "${PWD}/config" ]]; then
-    mkdir -p "${PWD}/config"
-    cp "${PWD}/.devcontainer/configuration.yaml" "${PWD}/config/configuration.yaml"
-    ${PWD}/.venv/bin/hass --config "${PWD}/config" --script ensure_config
+if [[ ! -d "${WORKSPACE_DIR}/config" ]]; then
+    mkdir -p "${WORKSPACE_DIR}/config"
+    cp "${WORKSPACE_DIR}/.devcontainer/configuration.yaml" "${WORKSPACE_DIR}/config/configuration.yaml"
+    ${WORKSPACE_DIR}/.venv/bin/hass --config "${WORKSPACE_DIR}/config" --script ensure_config
 fi
 
-export PYTHONPATH="${PYTHONPATH}:${PWD}/custom_components"
+export PYTHONPATH="${PYTHONPATH}:${WORKSPACE_DIR}/custom_components"
