beb4242f8f1225185f445b591d3f8531efc8141f
strawgate <williamseaston@gmail.com>
2024-06-06 18:27:04 +0000

    remove diagnostics from config_flow

--- custom_components/elasticsearch/config_flow.py
+++ custom_components/elasticsearch/config_flow.py
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry, ConfigFlowResult
 from homeassistant.const import (
     CONF_ALIAS,
@@ -663,12 +662,3 @@ class ElasticOptionsFlowHandler(config_entries.OptionsFlow):
             domains.add(state.domain)
 
         return sorted(domains), sorted(entity_ids)
-
-
-async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry) -> dict[str, any]:
-    """Return diagnostics for the config entry."""
-
-    return {
-        "data": async_redact_data(entry.data, CONFIG_TO_REDACT),
-        "options": async_redact_data(entry.options, CONFIG_TO_REDACT),
-    }
--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -18,11 +18,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
-from pytest_homeassistant_custom_component.typing import ClientSessionGenerator
 from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
 
-from custom_components.elasticsearch.config_flow import async_get_config_entry_diagnostics
 from custom_components.elasticsearch.const import (
     CONF_INDEX_FORMAT,
     CONF_INDEX_MODE,
@@ -701,30 +699,3 @@ async def test_legacy_options_flow(hass: HomeAssistant, es_aioclient_mock: Aioht
     )
 
     assert options_result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-
-async def test_entry_diagnostics(
-    hass: HomeAssistant,
-    hass_client: ClientSessionGenerator,
-    snapshot: SnapshotAssertion,
-) -> None:
-    """Test config entry diagnostics."""
-
-    entry = MockConfigEntry(
-        unique_id="test_legacy_options",
-        domain=DOMAIN,
-        version=3,
-        data={
-            "url": "https://localhost:9200",
-            "username": "original_user",
-            "password": "abc123",
-            CONF_PUBLISH_MODE: PUBLISH_MODE_ALL,
-            CONF_ALIAS: "hass-events",
-            CONF_INDEX_MODE: INDEX_MODE_LEGACY,
-            CONF_INDEX_FORMAT: "hass-events",
-            "use_connection_monitor": False,
-        },
-        title="ES Config",
-    )
-
-    diagnostics = await async_get_config_entry_diagnostics(hass, entry)
-    assert diagnostics == snapshot
